3702521f4edcf8b3b5fc1f028eec3e8c117f4214
Naveena R <110770588+Nave-ena@users.noreply.github.com>
2023-06-14 12:23:57 +0530

    com1

---
+++ email & name from db.py
@@ -0,0 +1,30 @@
+import requests
+
+# Replace the placeholder with your Xano endpoint URL
+ENDPOINT_URL = 'https://x8ki-letl-twmt.n7.xano.io/api:5OydBuyZ/unread_user'
+
+try:
+    response = requests.get(ENDPOINT_URL)
+    data = response.json()
+
+    # Check if the response is a list/array
+    if isinstance(data, list):
+        # Iterate over each record in the response
+        for record in data:
+            name = record.get('name')
+            email = record.get('email')
+            link = record.get('link')
+            summary = record.get('summary')
+
+            # Print the fetched data
+            print("Name:", name)
+            print("Email:", email)
+            print("Link:", link)
+            print("Summary:", summary)
+            print()
+    else:
+        # Handle non-array response
+        print("Invalid response format. Expected an array.")
+
+except requests.exceptions.RequestException as e:
+    print('Error occurred:', e)
